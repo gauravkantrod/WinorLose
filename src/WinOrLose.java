1b0e0a5220256624785457dc944e3d85832fd124
Gaurav Kantrod <gaurav.kantrod@vodafone.com>
2019-04-27 15:35:32 +0530

    Removing comments and code refactor.

--- src/WinOrLose.java
+++ src/WinOrLose.java
@@ -8,26 +8,17 @@ public class WinOrLose {
 	public static void main(String[] args) {
 
 		Scanner in = new Scanner(System.in);
-		// System.out.println("numberOfTestCases");
+
 		int numberOfTestCases = in.nextInt();
-		// System.out.println("You entered numberOfTestCases " + numberOfTestCases);
 
 		for (int j = 0; j < numberOfTestCases; j++) {
 			int numberOfPlayersAndVillian = in.nextInt();
-			// System.out.println("You entered numberOfPlayersAndVillian " +
-			// numberOfPlayersAndVillian);
 
-			// System.out.println("******************** Enter villian's powers
-			// *****************************");
 			int[] villianArray = new int[numberOfPlayersAndVillian];
 			for (int i = 0; i < numberOfPlayersAndVillian; i++) {
 				villianArray[i] = in.nextInt();
 			}
 
-			// use array length check if unable to compile all the test cases.
-
-			// System.out.println("********************** Enter players's powers
-			// ****************************");
 			int[] playerArray = new int[numberOfPlayersAndVillian];
 			for (int i = 0; i < numberOfPlayersAndVillian; i++) {
 				playerArray[i] = in.nextInt();
@@ -35,20 +26,6 @@ public class WinOrLose {
 
 			Arrays.sort(villianArray);
 			Arrays.sort(playerArray);
-
-			/*
-			 * System.out.
-			 * println("\n\n************************ Standard output ******************************"
-			 * ); System.out.
-			 * println("***************************** Villian Array *******************************"
-			 * ); for (int i = 0; i < numberOfPlayersAndVillian; i++) { System.out.print(" "
-			 * + villianArray[i]); }
-			 * 
-			 * System.out.
-			 * println("**************************** Player Array *********************************"
-			 * ); for (int i = 0; i < numberOfPlayersAndVillian; i++) { System.out.print(" "
-			 * + playerArray[i]); }
-			 */
 
 			List<Integer> winningStatus = new ArrayList<Integer>(numberOfPlayersAndVillian);
 
